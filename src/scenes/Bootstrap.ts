3caa87a40c01d2ddba5bfda6b2b1a8a2d8024047
heesankim <opqryt45@gmail.com>
2023-01-14 04:10:52 +0900

    feat : Perfect 맵 구현중( 전체적인 디자인)

--- src/scenes/Bootstrap.ts
+++ src/scenes/Bootstrap.ts
@@ -27,19 +27,125 @@ export default class Bootstrap extends Phaser.Scene {
     this.load.image('backdrop_night', 'assets/background/backdrop_night.png');
     this.load.image('sun_moon', 'assets/background/sun_moon.png');
 
-    this.load.tilemapTiledJSON('tilemap', 'assets/map/game_map.json');
+    this.load.tilemapTiledJSON('tilemap', 'assets/map/huntingMap.json');
 
-    this.load.spritesheet('interior', 'assets/map/interior.png', {
+    this.load.spritesheet('interior', 'assets/tileset/interior.png', {
       frameWidth: 16,
       frameHeight: 16,
     });
 
-    this.load.spritesheet('tiles', 'assets/map/tiles.png', {
+    this.load.spritesheet('basement', 'assets/tileset/basement.png', {
       frameWidth: 16,
       frameHeight: 16,
     });
 
-    this.load.spritesheet('buildings', 'assets/map/buildings.png', {
+    this.load.spritesheet('bigTree', 'assets/tileset/bigTree.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('boat', 'assets/tileset/boat.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('camping', 'assets/tileset/camping.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+
+    this.load.spritesheet('camping3', 'assets/tileset/camping3.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('campingZone', 'assets/tileset/campingZone.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.spritesheet('waterBridge', 'assets/tileset/waterBridge.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+      });
+
+    // food 이미지 너무커서 vehicles 에 다 있음
+
+    this.load.spritesheet('generic', 'assets/tileset/generic.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('boat1', 'assets/animate/boat1.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+    this.load.spritesheet('billboard', 'assets/animate/billboard.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('campfire2', 'assets/tileset/campfire2.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('genericBuildings', 'assets/tileset/genericBuildings.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('picnic', 'assets/tileset/picnic.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('playground', 'assets/tileset/playground.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('tent', 'assets/tileset/tent.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('modernExteriors', 'assets/tileset/modernExteriors.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('floorTiles', 'assets/tileset/floorTiles.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('fences', 'assets/tileset/fences.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('villas', 'assets/tileset/villas.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('vehicles', 'assets/tileset/vehicles.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('waterToy', 'assets/tileset/waterToy.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('tiles', 'assets/tileset/tiles.png', {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
+
+    this.load.spritesheet('buildings', 'assets/tileset/buildings.png', {
       frameWidth: 16,
       frameHeight: 16,
     });
