9c4cb59b14bd3db9ae502c92ce11384d3a462b35
suyeon woo <woosean999@gmail.com>
2023-01-18 17:21:16 +0900

    Revert " build. socket port -> 4711"
    This reverts commit 959685b26e973a1f769ebb80d830b33d8ca7f27a.

--- src/services/Network2.ts
+++ src/services/Network2.ts
@@ -11,7 +11,7 @@ export default class chatNetwork {
     const socketUrl =
       process.env.NODE_ENV === 'production' || import.meta.env.VITE_SERVER === 'PRO'
         ? `https://${import.meta.env.VITE_SERVER_URL}`
-        : `http://${window.location.hostname}:4711`;
+        : `http://${window.location.hostname}:5002`;
 
     this.socketClient = io(`${socketUrl}`);
 
