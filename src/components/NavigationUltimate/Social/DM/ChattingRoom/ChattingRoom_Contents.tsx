eb1d67870d84173e5dde30344c6a766233cd9dd9
suyeon woo <woosean999@gmail.com>
2023-01-21 23:50:29 +0900

    feat. join-room 이벤트에 old-messages 받아오는 콜백함수 추가

--- src/components/NavigationUltimate/Social/DM/ChattingRoom/ChattingRoom_Contents.tsx
+++ src/components/NavigationUltimate/Social/DM/ChattingRoom/ChattingRoom_Contents.tsx
@@ -17,63 +17,57 @@ const Wrapper = styled.div`
 
 export default function ChatBubbles(props) {
   const game = phaserGame.scene.keys.game as Game;
-  const socketClient = game.networt2.getSocket();
-  // 기존 메세지 리스트 -> 삭제 예정
-  const [messageList, setMessageList] = useState<any>([]);
+  const socketNetwork = game.networt2;
+  const socketClient = socketNetwork.getSocket();
 
   // 채팅 시작 시 저장되어 있던 채팅 리스트 보여줌
   const roomId = useAppSelector((state) => state.dm.roomId);
   const friendId = useAppSelector((state) => state.dm.friendId);
   const userId = useAppSelector((state) => state.user.userId);
 
-  socketClient.emit('join-room', { roomId: roomId, userId: userId, friendId: friendId });
+  // 기존 메세지 리스트 -> 삭제 예정
+  const [messageList, setMessageList] = useState<any>([]);
 
-  socketClient.on('show-messages', (data) => {
-    // 기존 메시지들
-    data.forEach((element) => {
-      if (element.senderId) {
-        if (element.senderId === userId) {
-          element.id = 0;
-        } else {
-          element.id = 1;
-        }
-        setMessageList((messageList) => [...messageList, element]);
-      }
-    });
-  });
+  useEffect(() => {
+    console.log('마운트');
+    const callback = (oldMessages) => {
+      setMessageList(oldMessages);
+    };
+    socketNetwork.joinRoom(roomId, userId, friendId, callback);
+  }, []);
 
   // 실시간 메세지 받으면 채팅 리스트에 추가
-  socketClient.on('message', (data) => {
-    data.id = 1;
-    const newMessageList = [...messageList, data];
-    setMessageList(newMessageList);
-    // setMessageList((messageList) => [...messageList, data]);
-  });
+  // socketClient.on('message', (data) => {
+  //   data.id = 1;
+  //   // const newMessageList = [...messageList, data];
 
+  //   // setMessageList((messageList) => [...messageList, data]);
+  // });
+  // }, []);
   // 내가 쓴 메세지 채팅 리스트에 추가
-  useEffect(() => {
-    if (!props.newMessage || props.newMessage.length === 0) return;
+  // useEffect(() => {
+  //   if (!props.newMessage || props.newMessage.length === 0) return;
 
-    const body = {
-      // id : 0,
-      roomId: roomId,
-      userId: userId,
-      friendId: friendId,
-      message: props.newMessage.message,
-    };
+  //   const body = {
+  //     // id : 0,
+  //     roomId: roomId,
+  //     userId: userId,
+  //     friendId: friendId,
+  //     message: props.newMessage.message,
+  //   };
 
-    setMessageList((messageList) => [...messageList, props.newMessage]);
+  //   setMessageList((messageList) => [...messageList, props.newMessage]);
 
-    // 내가 쓴 메세지 서버에 전송
-    game.networt2.sendMessage(body);
-  }, [props.newMessage]);
+  //   // 내가 쓴 메세지 서버에 전송
+  //   game.networt2.sendMessage(body);
+  // }, [props.newMessage]);
 
   return (
     <>
       <Wrapper>
         <ChatFeed
           maxHeight={450}
-          messages={messageList} // Array: list of message objects
+          messages={messageList || []} // Array: list of message objects
           // isTyping={false} // Boolean: is the recipient typing
           // hasInputField={false} // Boolean: use our input, or use your own
           // showSenderName={'who'} // show the name of the user who sent the message
