c188646d7829c7ae0aba7cf8fa09f718f7296a52
suyeon woo <woosean999@gmail.com>
2023-01-21 22:08:15 +0900

    refactor CattingRoomBototm - 안쓰는 변수 정리

--- src/components/NavigationUltimate/Social/DM/ChattingRoom/ChattingRoom_Bottom.tsx
+++ src/components/NavigationUltimate/Social/DM/ChattingRoom/ChattingRoom_Bottom.tsx
@@ -15,12 +15,7 @@ import Colors from 'src/utils/Colors';
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
 export default function BottomAppBar(props) {
-  const [inputValue, setInputValue] = useState('');
-  const [readyToSubmit, setReadyToSubmit] = useState(false);
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLInputElement>(null);
   const dispatch = useAppDispatch();
-  const defaultInput = useRef<HTMLInputElement>(null);
   const { setNewMessage } = props;
   const focused = useAppSelector((state) => state.chat.focused);
 
