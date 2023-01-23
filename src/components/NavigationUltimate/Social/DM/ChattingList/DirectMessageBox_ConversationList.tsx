32fe67de65eb875446886605e44df80b5622030a
suyeon woo <woosean999@gmail.com>
2023-01-23 16:47:34 +0900

    feat. 수락/거절 모달 복구

--- src/components/NavigationUltimate/Social/DM/ChattingList/DirectMessageBox_ConversationList.tsx
+++ src/components/NavigationUltimate/Social/DM/ChattingList/DirectMessageBox_ConversationList.tsx
@@ -102,7 +102,7 @@ export const ConversationList = () => {
             ) {
               room.unreadCount! += 1;
             }
-            if (newMessageCnt === 0) {
+            if (room.status !== IChatRoomStatus.FRIEND_REQUEST && newMessageCnt === 0) {
               room.unreadCount = 0;
             }
 
